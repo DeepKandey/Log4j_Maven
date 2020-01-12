d97e8fae59d79d16a3cd6f80bd39bb3618dac7e7
DeepKandey <deepakggsipu@gmail.com>
2020-01-12 18:29:37 +0530

    allure report implementation

--- src/test/java/com/tests/TestHelloExample.java
+++ src/test/java/com/tests/TestHelloExample.java
@@ -5,27 +5,21 @@ import org.testng.annotations.Test;
 
 import com.utils.LoggerUtil;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+
 public class TestHelloExample {
-	
-		/*public static void main(String[] args) {
-			HelloExample2 obj = new HelloExample2();
-			obj.runMe("mkyong");
-		}*/
-		
-	@Test	
-	private void runMe(){
-			/*if(logger.isDebugEnabled()){
-				logger.debug("This is debug : " + parameter);
-			}
-			
-			if(logger.isInfoEnabled()){
-				logger.info("This is info : " + parameter);
-			}*/
-			
-			/*logger.warn("This is warn : " + parameter);
-			logger.error("This is error : " + parameter);
-			logger.fatal("This is fatal : " + parameter);*/		
-			LoggerUtil.logMessage("Checking LoggerUtil abstract class ");
-			Assert.assertEquals(true, false);
-		}
+
+	@Test(priority = 1, description = "Simple test case to check Logger")
+	@Severity(SeverityLevel.NORMAL)
+	@Description("test case to check allure report")
+	@Story("Story to use allure report")
+	@Step("This is just demo method")
+	private void runMe() {
+		LoggerUtil.logMessage("Checking LoggerUtil abstract class ");
+		Assert.assertEquals(false, false);
 	}
+}
