e333c23b9d1c24033e32fff3e3174fc5643e6a54
DeepKandey <deepakggsipu@gmail.com>
2020-01-24 09:17:50 +0530

    added allure reporting

--- src/test/java/com/tests/TestHelloExample.java
+++ src/test/java/com/tests/TestHelloExample.java
@@ -1,8 +1,12 @@
 package com.tests;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.BasePage;
+import com.utils.AllureReportListener;
 import com.utils.LoggerUtil;
 
 import io.qameta.allure.Description;
@@ -11,7 +15,11 @@ import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 
-public class TestHelloExample {
+@Listeners(AllureReportListener.class)
+public class TestHelloExample extends BasePage {
+
+	public BasePage basePage;
+	public WebDriver driver;
 
 	@Test(priority = 1, description = "Simple test case to check Logger")
 	@Severity(SeverityLevel.NORMAL)
@@ -20,6 +28,10 @@ public class TestHelloExample {
 	@Step("This is just demo method")
 	private void runMe() {
 		LoggerUtil.logMessage("Checking LoggerUtil abstract class ");
-		Assert.assertEquals(false, false);
+		basePage = new BasePage();
+		driver = basePage.initialize_driver();
+		driver.get("https://www.google.com");
+		Assert.assertEquals(false, true);
+		driver.quit();
 	}
 }
